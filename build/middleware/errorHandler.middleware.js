b9eb8661828f9295398c06a4090d7fe5b6c0435a
swethusiast <aitc.dev@gmail.com>
2021-12-19 03:55:14 +0200

    done!

--- build/middleware/errorHandler.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var errorHandler = function (err, req, res, next) {
-    var status = err.status || 500;
-    var message = err.message || 'Something went wrong';
-    res.status(status).json({ status: status, message: message });
-};
-exports.default = errorHandler;
